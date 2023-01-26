d3b588f7fb0634466817c23200e3f0e6713d8f10
ElenaAgeeva2505 <agatamisteri2505@mail.ru>
2023-01-26 21:11:28 +0300

    RxJs

--- Task11/src/app/app.component.ts
+++ Task11/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { CounterService } from './counter.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Task11';
+  constructor(public counterService: CounterService) {}
+
+  counterSubs$!: Subscription;
+  enableStopButton = false;
+
+  getCounter() {
+    this.counterSubs$ = this.counterService.setCounter().subscribe((next) => {
+      console.log(next);
+    });
+    this.enableStopButton = true;
+  }
+
+  stopSubs() {
+    this.counterSubs$.unsubscribe();
+    this.enableStopButton = false
+  }
 }
--- Task11/src/app/app.module.ts
+++ Task11/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterService } from './counter.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [CounterService],
 })
-export class AppModule { }
+export class AppModule {}
